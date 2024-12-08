c1dce23e6475ba53f2567356d3456b6c2429bb04
Germo Aabla <germo.aabla.it@gmail.com>
2024-12-08 20:22:49 +0200

    Week6StringsAndArrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string[] fruitBasket = { "apples", "bananas", "oranges" };
+
+Console.WriteLine($"There are {fruitBasket.Length} fruit in your basket");
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
+
+fruitBasket[0] = "pine-apple";
+fruitBasket[2] = "peaches";
+
+Console.WriteLine();
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿//string myString = "Hello, World!";
+
+////int theLengthOfMyString = myString.Length;
+////Console.WriteLine(theLengthOfMyString);
+//Console.WriteLine(myString.Length);
+
+
+//Console.WriteLine("Enter your first name:");
+//string userName = Console.ReadLine().Trim(); //trim removes space before and after string; not inbetween!
+
+string userName = "Harry Potter";
+Console.WriteLine(userName);
+
+Console.WriteLine(userName.Length);
+Console.WriteLine($"The last letter in your name is {userName[userName.Length - 1]}");
+
+
+//a-counter
+
+
+//int counter = 0;
+
+//for  (int i = 0; i < userName.Length; i++)
+//{
+//    if (userName[i] == 'a')
+//    {
+//        counter++; //counter = counter + 1
+//    }
+//}
+
+//Console.WriteLine($"Your name has {counter} a-letters.");
+
+string updatedUsername = userName.Replace('a', '4').Replace('o', '0').Replace('e', '3');
+
+Console.WriteLine(updatedUsername);
